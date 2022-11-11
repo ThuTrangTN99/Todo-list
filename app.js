92eb67cc983afce8b37d53a16fff486cef02eba5
ThuTrangTN99 <thutrangtn1999@gmail.com>
2022-11-11 11:27:14 +0700

    update

--- app.js
+++ app.js
@@ -1,19 +1,31 @@
-let todos = [];
-let getLocalStorageData = localStorage.getItem("New todo");
+// let todos = [];
 const form = document.querySelector("#form")
 const input = document.querySelector("#input")
 const todoList = document.querySelector("#todo-list")
+const clearAll = document.querySelector("#clear")
+// chạy mỗi khi reload
+window.onload = function () {
+    showTodos();
+}
+//them todo
 form.addEventListener("submit", (e) => {
     e.preventDefault();
     const valueInput = input.value;
+    const localStore = localStorage.getItem("todos") ? JSON.parse(localStorage.getItem("todos")) : [];
+    // const newTodos = {
+    //     name: valueInput,
+    //     iscompleted: false,
+    //     id: todos.length,
+    // }; // khi k dùng localstorage
     const newTodos = {
         name: valueInput,
         iscompleted: false,
-        id: todos.length,
+        id: localStore.length,
     };
     //them todo
     // todos.push(newTodos); cách này dùng khi khai báo bằng const
-    todos = [...todos, newTodos]; //khi khai báo bằng let
+    // todos = [...todos, newTodos]; //khi khai báo bằng let và k dùng localstorage
+    localStorage.setItem("todos", JSON.stringify([...localStore, newTodos]))
     showTodos();
 
     //clearr & input
@@ -22,8 +34,9 @@ form.addEventListener("submit", (e) => {
 //tạo ra hàm để render 
 const showTodos = () => {
     //cách reset sau mỗi lần tạo todo
+    const localStore = localStorage.getItem("todos") ? JSON.parse(localStorage.getItem("todos")) : [];
     todoList.innerHTML = "";
-    todos.forEach((item) => {
+    localStore.forEach((item) => {
         const divTodo = document.createElement("div")
         divTodo.innerHTML = `
         <p class="todo">${item.name}</p>
@@ -39,12 +52,25 @@ const showTodos = () => {
 // xóa todo
 
 const handleDelete = (id) => {
-    todos = todos.filter((t) => t.id !== id)
+    const localStore = localStorage.getItem("todos") ? JSON.parse(localStorage.getItem("todos")) : [];
+    const newlocalStore = localStore.filter((t) => t.id !== id)
+    //lưu
+    localStorage.setItem("todos", JSON.stringify(newlocalStore))
     showTodos();
 }
 
 //make completed
 const handleCompleted = (id) => {
-    todos = todos.map(el => el.id === id ? { ...el, iscompleted: true } : el)
+    const localStore = localStorage.getItem("todos") ? JSON.parse(localStorage.getItem("todos")) : [];
+    const newlocalStore = localStore.map(el => el.id === id ? { ...el, iscompleted: true } : el)
+
+    //lưu 
+    localStorage.setItem("todos", JSON.stringify(newlocalStore))
     showTodos();
 }
+
+//xóa tất cả
+clearAll.addEventListener("click", () => {
+    localStorage.removeItem("todos");
+    showTodos();
+})
